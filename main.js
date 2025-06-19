4e32446c7bd20cf2493a6585fcbb95a00a6ca626
Shinyi Li <shinyili@Shinyis-MacBook-Air.local>
2025-06-19 16:15:33 -0400

    all times working

--- main.js
+++ main.js
@@ -31,30 +31,3 @@ app.whenReady().then(() => {
 function closeHandler() {
   BrowserWindow.getCurrentWindow().close();
 }
-
-/*
-// trying timer part
-// Set the initial time (5 minutes = 300 seconds)
-let timeLeft = 180;
-
-// Function to update the countdown display
-function updateCountdown() {
-  const minutes = Math.floor(timeLeft / 60);
-  const seconds = timeLeft % 60;
-
-  // Format to "MM:SS" (e.g., 04:59)
-  const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds
-    .toString()
-    .padStart(2, '0')}`;
-  document.querySelector('.countdown').textContent = formattedTime;
-
-  // Decrease time by 1 second
-  timeLeft--;
-
-  // Stop the timer when it reaches 0
-  if (timeLeft < 0) {
-    clearInterval(timer);
-    document.querySelector('.countdown').textContent = '00:00';
-  }
-}
- */
